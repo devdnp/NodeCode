d3da1b52115dc3ceace4678fa7226c3cdea86e66
durgeshvipul <durgeshpandey419@gmail.com>
2022-11-26 08:03:47 +0530

    Module is used

---
+++ NodeCode/modUsed.js
@@ -0,0 +1,10 @@
+const myFun = require('./mymod')
+
+console.log(myFun(12,45,33));
+
+console.log("First line to execute");
+let a=2;
+const b=3;
+var c=5;
+console.log(a+b+c);
+console.log("The node js example for basics only")
